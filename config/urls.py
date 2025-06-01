8c31d135be8eb14b1369bf4b37634bb134abe4b3
mexic <mexic2284@mail.ru>
2025-06-01 20:16:48 +0400

    add path to swagger, users + urlpatterns += static

--- config/urls.py
+++ config/urls.py
@@ -15,9 +15,25 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 
+from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
+
+from config import settings
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path("schema/", SpectacularAPIView.as_view(), name="schema"),
+    path(
+        "swagger/",
+        SpectacularSwaggerView.as_view(url_name="schema"),
+        name="swagger-ui",
+    ),
+    path(settings.API_VERSION, include("users.urls", namespace="users")),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(
+        settings.MEDIA_URL, document_root=settings.MEDIA_ROOT
+    )
