c350ca717e06bd54ba640499f97cacededbf997a
mexic <mexic2284@mail.ru>
2025-06-01 20:16:13 +0400

    user model is registered in the admin panel

--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,36 @@
 from django.contrib import admin
 
 # Register your models here.
+from django.contrib import admin
+
+from users.models import User
+
+
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    """
+    Регистрация модели в админ панели
+    """
+
+    list_display = ("email", "username", "is_staff", "is_active")
+    fieldsets = (
+        (None, {"fields": ("email", "password")}),
+        (
+            "Personal info",
+            {
+                "fields": (
+                    "username",
+                    "phone_number",
+                    "country",
+                    "avatar",
+                    "token",
+                )
+            },
+        ),
+        (
+            "Permissions",
+            {
+                "fields": ("is_active", "is_staff", "is_superuser"),
+            },
+        ),
+    )
