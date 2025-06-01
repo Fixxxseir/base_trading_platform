a37ca4211188eff4cfe9acff18a7f012a73a89de
mexic <mexic2284@mail.ru>
2025-06-02 02:14:33 +0400

    create viewset for NetworkNode

--- trading_platform/views.py
+++ trading_platform/views.py
@@ -1,3 +1,24 @@
-from django.shortcuts import render
+from rest_framework import viewsets, permissions, filters
+from django_filters.rest_framework import DjangoFilterBackend
+from .models import NetworkNode
+from .serializers import NetworkNodeSerializer
+from django.contrib.auth.models import User
 
-# Create your views here.
+
+class IsActiveUser(permissions.BasePermission):
+    def has_permission(self, request, view):
+        return request.user and request.user.is_active
+
+
+class NetworkNodeViewSet(viewsets.ModelViewSet):
+    queryset = NetworkNode.objects.select_related(
+        "contact", "supplier", "owner"
+    ).prefetch_related("products")
+    serializer_class = NetworkNodeSerializer
+    permission_classes = [IsActiveUser]
+    filter_backends = [DjangoFilterBackend]
+    filterset_fields = {
+        "contact__country": ["exact"],
+        "level": ["exact"],
+        "created_at": ["gte", "lte"],
+    }
